35dc02a46f73e0f818f423a60e473391a20c2185
Coderexe05 <atishay.corp@gmail.com>
2026-06-18 23:39:14 +0530

    y-app12/

---
+++ my-app12/src/App.jsx
@@ -0,0 +1,44 @@
+// function App(){
+//     return(
+//     <div className="App">
+//         <h1>Hello World</h1>
+//         <p>This is my first react app.</p>
+//         {/* <Cse12 /> */}
+//         </div>
+//     );    
+// }
+
+// //2nd component
+// function CSE12(){
+//   return(
+//   <div className="CSE12">
+//     <h2>Welcwme to CSE12</h2>
+//     <p>This is the second component.</p>
+//     </div>
+//   );
+// }
+// export default App;
+
+// import Student from"./Student";
+
+// function App(){
+
+//   return(
+//     <div className="App">
+//       <h1>Student Information</h1>
+//       <Student name="Atishaya" course="CSE12" marks="99">
+//       </Student>
+//     </div>
+//   );
+
+// }
+
+function App(){
+  return(
+    <div>
+      <h1>Student Card</h1>
+      <StudentCard name="props" marks="30" grade="A"/>
+    </div>
+  );
+  export default App;
+}
---
+++ my-app12/src/studentcard.jsx
@@ -0,0 +1,7 @@
+function StudentCard(){
+    <div>
+        <h2>Name</h2>{props.name}
+        <p>Marks</p>{props.marks}
+        <p>Grade</p>{props.grade}
+    </div>
+}
